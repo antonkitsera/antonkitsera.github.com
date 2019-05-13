e8896fd354482d8466fef1eb9026d61a2a91c689
Anton Kitsera <antonkitsera@gmail.com>
2019-05-13 09:29:23 +0300

    Added

---
+++ binariks/precache-manifest.a522d10df88f926cda446e1972d4bb99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991d96a621ae72a07cb1e70de1a36a68",
+    "url": "/binariks/index.html"
+  },
+  {
+    "revision": "471536958895d50d7041",
+    "url": "/binariks/static/css/main.b958d79d.chunk.css"
+  },
+  {
+    "revision": "020296198e488a24d155",
+    "url": "/binariks/static/js/2.6d11a779.chunk.js"
+  },
+  {
+    "revision": "471536958895d50d7041",
+    "url": "/binariks/static/js/main.e432aaaf.chunk.js"
+  },
+  {
+    "revision": "62b3691e4e315825f47a",
+    "url": "/binariks/static/js/runtime~main.323ebbad.js"
+  }
+]);
